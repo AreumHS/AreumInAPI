9a37b45547074cd53c33f4b3f636e3332b807e2d
박성훈 / Koder <koder0205@gmail.com>
2021-05-30 02:01:40 +0900

    update
    수정 살짝

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const express   = require('express');
-const dotenv    = require('dotenv').config();
-const mysql     = require('mysql');
-const crypto    = require('crypto');
+const express    = require('express');
+const dotenv     = require('dotenv').config();
+const mysql      = require('mysql');
+const crypto     = require('crypto');
+const bodyparser = require('body-parser');
 
 const port = 8081;
 const host = '0.0.0.0';
@@ -19,18 +20,23 @@ const db = mysql.createConnection({
 db.connect();
 const app = express();
 
-function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
+app.use(express.json());
+app.use(express.urlencoded( {extended : false } ));
+
+function isNumber(n)    { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
 function isEmail(email) { return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email); }
 
 /////////////// get -> post , req.query.xx -> req.body.xx ////////////////
 
-app.post('/register', (req,res) => {
+app.post('/register', function(req,res){ // 회원가입 API
     var id     = req.body.id;
     var name   = req.body.name;
     var pw     = req.body.pw;
     var pw2    = req.body.pw2;
     var email  = req.body.email;
     var code   = req.body.code;
+
+    console.log(req.body);
 
     if( id    == null ||
         name  == null ||
@@ -55,14 +61,16 @@ app.post('/register', (req,res) => {
         else{
             db.query('INSERT INTO `student-code`(`id`, `name`, `password`, `email`) VALUES ("'+pid+'","'+name+'","'+hash+'","'+email+'");', function(error, val){
                 if(error){ return res.status(400).json({status: "Failed", log:"DatabaseInsertErr"}); }
-                console.log(`${id} ${name}님이 성공적으로 회원가입 처리되었습니다.`);
-                return res.status(200).json({status: "Succeed", log:"RegisterDone", return: pid});
             });
+            /////////////// 다른 데이터베이스 세팅하는 파트 ////////////////////
+
+
+            /////////////// 다른 데이터베이스 세팅하는 파트 끝 //////////////////
+            console.log(`${id} ${name}님이 성공적으로 회원가입 처리되었습니다.`);
+            return res.status(200).json({status: "Succeed", log:"RegisterDone", return: pid});
         }
     });
 });
-
-app.get
 
 app.listen(port,host);
 console.log(`API서버 작동 시작.`);
