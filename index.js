8c9e749d7880f6c5f72becf7b021f409ca5ecf25
박성훈 / Koder <koder0205@gmail.com>
2021-05-30 01:10:33 +0900

    first commit
    첫 커밋
    회원가입기능 완성

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express   = require('express');
+const dotenv    = require('dotenv').config();
+const mysql     = require('mysql');
+const crypto    = require('crypto');
+
+const port = 8081;
+const host = '0.0.0.0';
+
+const db = mysql.createConnection({
+    host     : process.env.DBLINK,
+    port     : process.env.DBPORT,
+    user     : process.env.DBID,
+    password : process.env.DBPW,
+    database : process.env.DBNAME
+});
+
+db.connect();
+const app = express();
+
+function isNumber(n) { return /^-?[\d.]+(?:e-?\d+)?$/.test(n); }
+function isEmail(email) { return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email); }
+
+/////////////// get -> post , req.query.xx -> req.body.xx ////////////////
+
+app.post('/register', (req,res) => {
+    var id     = req.body.id;
+    var name   = req.body.name;
+    var pw     = req.body.pw;
+    var pw2    = req.body.pw2;
+    var email  = req.body.email;
+    var code   = req.body.code;
+
+    if( id    == null ||
+        name  == null ||
+        pw    == null ||
+        pw2   == null ||
+        email == null ||
+        code  == null
+    ){ return res.status(400).json({status: "Failed", log:"NullException"}); }
+
+    if(code != process.env.CODE){ return res.status(400).json({status: "Failed", log:"CodeCheckFailed"}); }
+
+    if(pw != pw2)                       { return res.status(400).json({status: "Failed", log:"PasswordIncorrect"}); }
+    if(!isNumber(id) || id.length!=5)   { return res.status(400).json({status: "Failed", log:"IdIncorrect"}); }
+    if(!isEmail(email))                 { return res.status(400).json({status: "Failed", log:"EmailIncorrect"}); }
+    
+    var hash = crypto.createHmac('sha256', process.env.SECRET).update(pw).digest('hex');
+    var pid  = crypto.createHmac('sha256', process.env.SECRET).update(id).digest('hex');
+
+    db.query("SELECT * FROM `student-code` WHERE `id`='"+pid+"'", function(err, ret){
+        if(err)                  { return res.status(400).json({status: "Failed", log:"DatabaseSelectErr"}); }
+        if(ret[0] != undefined)  { return res.status(400).json({status: "Failed", log:"AlreadyUsing"}); }
+        else{
+            db.query('INSERT INTO `student-code`(`id`, `name`, `password`, `email`) VALUES ("'+pid+'","'+name+'","'+hash+'","'+email+'");', function(error, val){
+                if(error){ return res.status(400).json({status: "Failed", log:"DatabaseInsertErr"}); }
+                console.log(`${id} ${name}님이 성공적으로 회원가입 처리되었습니다.`);
+                return res.status(200).json({status: "Succeed", log:"RegisterDone", return: pid});
+            });
+        }
+    });
+});
+
+app.get
+
+app.listen(port,host);
+console.log(`API서버 작동 시작.`);
