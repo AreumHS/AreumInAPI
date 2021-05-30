31dd0cf8d90b2d3c2e6878505ccefc93978c1008
박성훈 / Koder <koder0205@gmail.com>
2021-05-30 13:46:02 +0900

    로그인과 비밀번호 변경
    복붙해서 날먹하자 ON

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ app.post('/register', function(req,res){ // 회원가입 API
     var email  = req.body.email;
     var code   = req.body.code;
 
-    console.log(req.body);
-
     if( id    == null ||
         name  == null ||
         pw    == null ||
@@ -68,6 +66,56 @@ app.post('/register', function(req,res){ // 회원가입 API
             /////////////// 다른 데이터베이스 세팅하는 파트 끝 //////////////////
             console.log(`${id} ${name}님이 성공적으로 회원가입 처리되었습니다.`);
             return res.status(200).json({status: "Succeed", log:"RegisterDone", return: pid});
+        }
+    });
+});
+
+app.get('/login', function(req,res){ // 로그인 API
+    var id = req.query.id;
+    var pw = req.query.pw;
+
+    if(id==null || pw==null)          { return res.status(400).json({status: "Failed", log:"NullException"}); }
+    if(!isNumber(id) || id.length!=5) { return res.status(400).json({status: "Failed", log:"IdIncorrect"}); }
+
+    var pid  = crypto.createHmac('sha256', process.env.SECRET).update(id).digest('hex');
+    var hash = crypto.createHmac('sha256', process.env.SECRET).update(pw).digest('hex');
+
+    db.query("SELECT * FROM `student-code` WHERE `id`='"+pid+"'", function(err, ret){
+        if(err)                  { return res.status(400).json({status: "Failed", log:"DatabaseSelectErr"}); }
+        if(ret[0] == undefined)  { return res.status(400).json({status: "Failed", log:"IdIsntExist"}); }
+        else{
+            if(ret[0].password != hash){ return res.status(400).json({status: "Failed", log:"PasswordFalse"}); }
+
+            console.log(`${id} ${ret[0].name}님이 성공적으로 로그인 처리되었습니다.`);
+            return res.status(200).json({status: "Succeed", log:"LoginDone", return: pid});
+        }
+    });
+});
+
+app.get('/pw-change', function(req,res){ // 비밀번호 변경 API
+    var id = req.query.id;
+    var pw = req.query.pw;
+    var nextpw = req.query.nextpw;
+
+    if(id==null || pw==null || nextpw==null) { return res.status(400).json({status: "Failed", log:"NullException"}); }
+    if(!isNumber(id) || id.length!=5)        { return res.status(400).json({status: "Failed", log:"IdIncorrect"}); }
+
+    var pid      = crypto.createHmac('sha256', process.env.SECRET).update(id).digest('hex');
+    var hash     = crypto.createHmac('sha256', process.env.SECRET).update(pw).digest('hex');
+    var nexthash = crypto.createHmac('sha256', process.env.SECRET).update(nextpw).digest('hex');
+
+    db.query("SELECT * FROM `student-code` WHERE `id`='"+pid+"'", function(err, ret){
+        if(err)                  { return res.status(400).json({status: "Failed", log:"DatabaseSelectErr"}); }
+        if(ret[0] == undefined)  { return res.status(400).json({status: "Failed", log:"IdIsntExist"}); }
+        else{
+            if(ret[0].password != hash){ return res.status(400).json({status: "Failed", log:"PasswordFalse"}); }
+
+            db.query("UPDATE `student-code` SET `password`='"+nexthash+"' WHERE `id`='"+pid+"'", function(err, ret){
+                if(err) { return res.status(400).json({status: "Failed", log:"DatabaseUpdateErr"}); }
+            });
+
+            console.log(`${id} ${ret[0].name}님이 비밀번호를 변경하셨습니다.`);
+            return res.status(200).json({status: "Succeed", log:"PasswordChanged", return: pid});
         }
     });
 });
